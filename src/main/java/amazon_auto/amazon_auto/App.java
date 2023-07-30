a289d4b53f89d11ebc320077203c46f77a1b0a1f
yaswanthkakula <yaswanthkumarkakula@gmail.com>
2023-07-30 21:59:59 +0530

    amazonfullyauto

---
+++ src/main/java/amazon_auto/amazon_auto/App.java
@@ -0,0 +1,13 @@
+package amazon_auto.amazon_auto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
